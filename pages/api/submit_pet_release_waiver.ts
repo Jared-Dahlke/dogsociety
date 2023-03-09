9fcea8410185168140385392821664359d158813
Jaredd <jaredd@Jared-Dahlkes-Macbook.local>
2023-03-08 19:39:36 -0800

    :wq

--- pages/api/submit_pet_release_waiver.ts
+++ pages/api/submit_pet_release_waiver.ts
@@ -652,9 +652,9 @@ export default async function handler(req, res) {
 		)
 
 		try {
-			sesClient.send(sendEmailCommand)
+			await sesClient.send(sendEmailCommand)
 			if (process.env.NODE_ENV === 'production') {
-				sesClient.send(sendEmailCommand2)
+				await sesClient.send(sendEmailCommand2)
 			}
 		} catch (err) {
 			console.log('Error', err)
